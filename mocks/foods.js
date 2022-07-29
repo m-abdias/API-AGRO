8a8ce70255b57c98165e46405e0515088c3f647b
Mariana Abdias Gonçalves <mabdiasgoncalves@gmail.com>
2022-07-28 21:06:32 -0300

    :sparkles: Imagens Corrigidas

--- mocks/foods.js
+++ mocks/foods.js
@@ -2,7 +2,8 @@ const foods = [
   {
     id: 1,
     name: 'Alface',
-    image: 'alface.png',
+    image:
+      'https://images.squarespace-cdn.com/content/v1/5b8edfa12714e508f756f481/1543944726778-3R28J0BST06GRZCOF7UR/alface-crespa-verde-hidropo%CC%82nica.png?format=1000w',
     relativeHumidityPercentage: '95%',
     storageTime: '20 dias',
     storageTemperature: '0 graus',
@@ -11,7 +12,8 @@ const foods = [
   {
     id: 2,
     name: 'Maca',
-    image: 'maca.png',
+    image:
+      'https://imagensemoldes.com.br/wp-content/uploads/2018/07/Imagem-de-Frutas-Ma%C3%A7%C3%A3-5-PNG.png',
     relativeHumidityPercentage: '90%',
     storageTime: '30 semanas',
     storageTemperature: '3 graus',
@@ -20,7 +22,8 @@ const foods = [
   {
     id: 3,
     name: 'Abacate',
-    image: 'abacate.png',
+    image:
+      'https://www.pngmart.com/files/15/Avocado-Half-PNG-Free-Download.png',
     relativeHumidityPercentage: '85% a 90%',
     storageTime: '20 dias',
     storageTemperature: '10 graus',
@@ -29,7 +32,8 @@ const foods = [
   {
     id: 4,
     name: 'Banana',
-    image: 'banana.png',
+    image:
+      'https://cdn.pixabay.com/photo/2016/02/23/17/29/banana-1218133_1280.png',
     relativeHumidityPercentage: '90% a 95%',
     storageTime: '20 dias',
     storageTemperature: '15 graus',
@@ -38,7 +42,8 @@ const foods = [
   {
     id: 5,
     name: 'Mamão',
-    image: 'mamao.png',
+    image:
+      'https://supermercadomanos.com.br/wp-content/uploads/2017/07/Mamao-Papaia.png',
     relativeHumidityPercentage: '90% a 95%',
     storageTime: '20 dias',
     storageTemperature: '8 graus',
@@ -47,7 +52,8 @@ const foods = [
   {
     id: 6,
     name: 'Abacaxi',
-    image: 'abacaxi.png',
+    image:
+      'https://polpasefrutas.com.br/arquivos/imagens/produtos/mesa/abacaxi.png',
     relativeHumidityPercentage: '85% a 90%',
     storageTime: '20 dias',
     storageTemperature: '10 graus',
@@ -56,7 +62,8 @@ const foods = [
   {
     id: 7,
     name: 'Laranja',
-    image: 'laranja.png',
+    image:
+      'https://imagensemoldes.com.br/wp-content/uploads/2020/05/Laranja-PNG.png',
     relativeHumidityPercentage: '85% a 90%',
     storageTime: '3 meses',
     storageTemperature: '7 graus',
@@ -65,7 +72,8 @@ const foods = [
   {
     id: 8,
     name: 'Manga',
-    image: 'manga.png',
+    image:
+      'http://www.darosbr.com/site/wp-content/uploads/2014/12/mangakent3.png',
     relativeHumidityPercentage: '85% a 90%',
     storageTime: '20 dias',
     storageTemperature: '13 graus',
